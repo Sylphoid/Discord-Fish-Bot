5941668270c4b2fe5d3ebc5ac0f9b13cd7ce84b9
Justin <42817257+Sylphoid@users.noreply.github.com>
2019-08-09 18:13:41 -0700

    Lists and randomness
    two lists with 10 strings for randomness
    more fish commands, .fish1 and .fish2

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 import discord
 import os
+import random
 from keep_alive import keep_alive
 
 #create discord client, but what is a client?
 client = discord.Client()
+#bot = commands.Bot('.') #bot can do everything client does
 
+fishPossible = ['You caught a :fish:!',
+                'Hey that\'s a :crab:!',
+                'Fancy catch! A :tropical_fish:!',
+                ':blowfish: are poisonous, but sell for a lot!',
+                'Oh no! You caught a :dolphin:!',
+                'Wow you must be skilled to catch a :whale:!',
+                ':whale2: are hard to catch, but you got one!',
+                ':shrimp: nice one!',
+                ':squid: It looks really angry at you!',
+                'Oh dear. You caught a :shell:']
+
+fishWeight = ['You caught a :fish:!'] * 50 + ['Hey that\'s a :crab:!']* 10 +  ['Fancy catch! A :tropical_fish:!']* 5 + [':blowfish: are poisonous, but sell for a lot!']* 5 +  ['Oh no! You caught a :dolphin:!']* 1 +  ['Wow you must be skilled to catch a :whale:!']* 1 +  [':whale2: are hard to catch, but you got one!']* 1 + [':shrimp: nice one!']* 2 + [':squid: It looks really angry at you!']* 5 +  ['Oh dear. You caught a :shell:']* 20
+
+#:crab: :tropical_fish: :fish: :blowfish: :dolphin: :whale: :whale2: :shrimp: :squid: :shell: 
 @client.event
 async def on_ready():
     print("I'm in")
@@ -13,7 +29,13 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if message.content == '.fish':
-        await message.channel.send('You typed fish. :fish:')
+        for i in range(10):
+          await message.channel.send(fishPossible[random.randint(0, len(fishPossible) - 1)])
+    elif message.content == '.fish1':
+      await message.channel.send(fishPossible)
+    elif message.content == '.fish2':
+      for i in range(10):
+        await message.channel.send(random.choice(fishWeight))
     elif message.content == '.cast':
         channel = client.get_channel(608815074622046211)
         await channel.send('You casted your rod.')
@@ -24,7 +46,7 @@ async def on_message(message):
 keep_alive()
 token = os.environ.get("DISCORD_BOT_SECRET")
 client.run(token)
-
+bot.run(token)
 
 from discord.ext import commands
 
