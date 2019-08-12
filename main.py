ddb461d6d9c54a931d8e9043309e0a72d42cb245
Justin <42817257+Sylphoid@users.noreply.github.com>
2019-08-12 15:20:58 -0700

    Updates to bot and other things
    - changed commands so all bot responses will push to #river 
    - removed crab, dolphin, whale, whale2, shrimp, squid from possibilities (return at later date)
    - added chance of no bite
    - added .helper
    - finally figured out why bot instance wasn't running (had to import commands and run.bot(token))
    - added "bot is typing" to simulate waiting for a bite to .wait
    - turned all functional client into functional bot

--- main.py
+++ main.py
@@ -1,69 +1,99 @@
 import discord
+from discord.ext import commands
 import os
 import random
+import asyncio
 from keep_alive import keep_alive
 
+# Reminders to a future self/internet fellow:
+# If possible, use bot instead of client. Bot is a subclass of client and can do everything client does (or it's supposed to but sometimes feels like it doesn't)
+# message.channel.send() will reply, channel.send() will reply to specific channel
+#if internet example of bot.run('token') has quotes, remove the quotes as it DNW
+
 #create discord client, but what is a client?
-client = discord.Client()
-#bot = commands.Bot('.') #bot can do everything client does
+#client = discord.Client() 
+bot = commands.Bot(command_prefix='.') 
 
 fishPossible = ['You caught a :fish:!',
-                'Hey that\'s a :crab:!',
                 'Fancy catch! A :tropical_fish:!',
                 ':blowfish: are poisonous, but sell for a lot!',
-                'Oh no! You caught a :dolphin:!',
-                'Wow you must be skilled to catch a :whale:!',
-                ':whale2: are hard to catch, but you got one!',
-                ':shrimp: nice one!',
-                ':squid: It looks really angry at you!',
-                'Oh dear. You caught a :shell:']
+                'Oh dear. You caught a :shell:',
+                'Not even a nibble...']
 
-fishWeight = ['You caught a :fish:!'] * 50 + ['Hey that\'s a :crab:!']* 10 +  ['Fancy catch! A :tropical_fish:!']* 5 + [':blowfish: are poisonous, but sell for a lot!']* 5 +  ['Oh no! You caught a :dolphin:!']* 1 +  ['Wow you must be skilled to catch a :whale:!']* 1 +  [':whale2: are hard to catch, but you got one!']* 1 + [':shrimp: nice one!']* 2 + [':squid: It looks really angry at you!']* 5 +  ['Oh dear. You caught a :shell:']* 20
 
-#:crab: :tropical_fish: :fish: :blowfish: :dolphin: :whale: :whale2: :shrimp: :squid: :shell: 
-@client.event
+fishWeight = ['You caught a :fish:!'] * 50 + ['Fancy catch! A :tropical_fish:!']* 5 + [':blowfish: are poisonous, but sell for a lot!']* 5 + ['Oh dear. You caught a :shell:']* 10 + ['Not even a nibble...'] * 100
+
+channel = bot.get_channel(608815074622046211) #this forces all responses to a certain channel, but every async can't find it
+
+@bot.event
 async def on_ready():
     print("I'm in")
-    print(client.user)
+    print(bot.user)
 
-@client.event
-async def on_message(message):
-    if message.content == '.fish':
-        for i in range(10):
-          await message.channel.send(fishPossible[random.randint(0, len(fishPossible) - 1)])
-    elif message.content == '.fish1':
-      await message.channel.send(fishPossible)
-    elif message.content == '.fish2':
-      for i in range(10):
-        await message.channel.send(random.choice(fishWeight))
-    elif message.content == '.cast':
-        channel = client.get_channel(608815074622046211)
-        await channel.send('You casted your rod.')
-#    elif message.author != client.user: #was part of tutorial
+# @bot.event
+# async def on_message(message):
+#     if message.author != bot.user: #was part of tutorial
 #        await message.channel.send(message.content[::-1]) #different from tutorial bc it's either using a rewrite or the versions are different
 
+@bot.command()
+async def fish(ctx): #gets channel, displays "bot is typing", waits for a random time, prints random string
+    channel = bot.get_channel(608815074622046211)
+    await channel.trigger_typing()
+    await asyncio.sleep(random.randint(0,10))
+    await channel.send(fishPossible[random.randint(0, len(fishPossible) - 1)])
 
-keep_alive()
-token = os.environ.get("DISCORD_BOT_SECRET")
-client.run(token)
-bot.run(token)
+@bot.command() #prints the list
+async def fish1(ctx):
+    channel = bot.get_channel(608815074622046211)
+    await channel.send(fishPossible)
 
-from discord.ext import commands
+@bot.command()
+async def fish2(ctx):
+    channel = bot.get_channel(608815074622046211)
+    await channel.trigger_typing()
+    await asyncio.sleep(random.randint(0,10))
+    await channel.send(random.choice(fishWeight))
+
+@bot.command()
+async def cast(ctx):
+    channel = bot.get_channel(608815074622046211)
+    await channel.send('You casted your rod.')
+
+@bot.command()
+async def helper(ctx): #figure out how to change the .help cmd
+    channel = bot.get_channel(608815074622046211)
+    await channel.send('You fool. Help does nothing.')
+
+@bot.command() #waiting example
+async def wait(ctx):
+    channel = bot.get_channel(608815074622046211)
+    await channel.trigger_typing()
+    await asyncio.sleep(random.randint(0,10))
+    await channel.send('beep boop waiting')
 
 
-bot = commands.Bot(command_prefix='!')
+# @bot.event
+# async def on_ready():
+#     print("I'm ready.")
+#     global target_channel
+#     target_channel = bot.get_channel("608815074622046211")
+# @bot.command()
+# async def send(*, message):
+#     global target_channel
+#     await bot.send_message(channel, message)
 
 @bot.command()
 async def test(ctx):
     await ctx.send('I heard you! {0}'.format(ctx.author))
 
-bot.run('token')
-
-# from discord.ext.commands import Bot
-# bot = Bot("!")
-
 # @bot.command()
-# async def test(ctx):
-#     await ctx.send("Command executed")
+# async def ping(ctx):
+#     await ctx.send('pong')
 
-# await bot.run("TOKEN")
+
+keep_alive() #web server that keeps bot alive until an hour of inactivity, use uptimerobot if desire 24/7 active
+
+token = os.environ.get("DISCORD_BOT_SECRET")
+bot.run(token)
+#client.run(token) #archaic
+
